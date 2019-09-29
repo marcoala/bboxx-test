95fa9f738d699d7d8686706ad0e3fe60459a773d
Marco Alabruzzo <marco.alabruzzo@gmail.com>
2019-09-29 22:46:38 +0100

    Move knights

--- modules/knights.py
+++ modules/knights.py
@@ -4,3 +4,13 @@ class Knight:
         self.name = name
         self.x = x
         self.y = y
+
+    def move(self, direction):
+        if direction == 'N':
+            self.y = self.y - 1
+        elif direction == 'S':
+            self.y = self.y + 1
+        elif direction == 'E':
+            self.x = self.x + 1
+        elif direction == 'W':
+            self.x = self.x - 1
--- modules/tests/test_knights.py
+++ modules/tests/test_knights.py
@@ -1,3 +1,5 @@
+import pytest
+
 from modules.knights import Knight
 
 
@@ -6,3 +8,19 @@ def test_Knight_creation():
     assert knight.name == 'A'
     assert knight.x == 3
     assert knight.y == 4
+
+
+@pytest.mark.parametrize(
+    'direction,expected_x,expected_y',
+    [
+        ('N', 3, 3),
+        ('S', 3, 5),
+        ('E', 4, 4),
+        ('W', 2, 4),
+    ],
+)
+def test_Knight_move(direction, expected_x, expected_y):
+    knight = Knight('A', 3, 4)
+    knight.move(direction)
+    assert knight.x == expected_x
+    assert knight.y == expected_y
